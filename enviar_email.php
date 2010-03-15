b41327d4933b01a4a71ce7882d3dba98d31e6548
Ana Carolina <tesmkt@gamil.com>
2010-03-15 20:37:54 -0300

    Adicionado ao enviar_email.php resposta amigavel p usuario

--- enviar_email.php
+++ enviar_email.php
@@ -1,9 +1,35 @@
-<?php 
 
-	$email= 'tesmkt@gmail.com';
-	$assunto= $_POST['assunto'];
-	$mensagem= $_POST['mensagem'];
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Contato</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
 	
-	@mail ('tesmkt@gmail.com','Enviar email pelo php',' Hello')
-
-?>
+</head>
+<body>
+	<div class="container">
+		<h1>Contato</h1>
+		
+		<?php 
+			error_reporting(0);
+			$email= 'tesmkt@gmail.com';
+			$assunto= $_POST['assunto'];
+			$mensagem= $_POST['mensagem'];
+	
+			if ( mail( $email, $assunto, $mensagem ) ):
+			
+		?>		
+		
+			<p>Seu e-mail foi enviado!</p>
+		
+		<?php else: ?>
+		
+			<p>Seu e-mail não foi enviado... Burra!!!</p>
+		
+		<?php endif; ?>
+			
+	</div>
+</body>
+</html>
